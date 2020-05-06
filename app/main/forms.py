077b5f1856ce00cc5936af974affae89b9811297
default-007 <brianokola@gmail.com>
2020-05-06 20:04:43 +0300

    Added the models for the commets and displayed them

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,3 +10,9 @@ class CommentForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Your Pitch', validators=[Required()])
+    # my_category = StringField('Category', validators=[Required()])
+    my_category = SelectField('Category', choices=[('Interview-Pitch','Interview Pitch'),('Product-Pitch','Product Pitch'),('Promotion-Pitch','Promotion Pitch'),('Business','Business'),('Academic','Academic'),('Political','Political'),('Technology','Technology'),('Health','Health')],validators=[Required()])
+    submit = SubmitField('Pitch It!')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import Comment, Pitch, User
-from .forms import CommentForm, UpdateProfile
+from .forms import CommentForm, UpdateProfile, PitchForm
 from .. import db, photos
 from flask_login import login_required
 
@@ -83,3 +83,56 @@ def comment(pitch_id):
         return redirect(url_for('.comment', pitch_id = pitch_id))
     return render_template('main.new_comment.html', comment_form =comment_form, pitch = pitch,all_comments=all_comments) 
 '''
+@main.route('/home', methods = ['GET', 'POST'])
+@login_required
+def home():
+    pitch_form = PitchForm()
+    
+    if pitch_form.validate_on_submit():
+        pitch = pitch_form.pitch.data
+        cat = pitch_form.my_category.data
+
+        new_pitch = Pitch(pitch_content=pitch, pitch_category = cat)
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.home'))
+
+    all_pitches = Pitch.get_all_pitches()
+
+    title = 'Home | One Minute Pitch'    
+    return render_template('home.html', title=title, pitch_form=pitch_form, pitches=all_pitches)
+    
+@main.route('/pitch/<int:id>',methods = ['GET','POST'])
+@login_required
+def pitch(id):
+    
+    my_pitch = Pitch.query.get(id)
+    comment_form = CommentForm()
+    
+
+    if id is None:
+        abort(404)
+
+    if comment_form.validate_on_submit():
+        comment_data = comment_form.comment.data
+        new_comment = Comment(comment_content = comment_data, pitch_id = id)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.pitch',id=id))
+
+    all_comments = Comment.get_comments(id)
+
+
+    # up_likes = UpVote.get_votes(id)
+    # down_likes = DownVote.get_downvotes(id)
+
+    title = 'Comment | One Minute Pitch'
+    return render_template('pitch.html',pitch = my_pitch, comment_form = comment_form, comments = all_comments, title = title)
+
+@main.route('/category/<cat>')
+def category(cat):
+    my_category = Pitch.get_category(cat)
+
+    title = f'{cat} category | One Minute Pitch'
+
+    return render_template('category.html', title=title, category=my_category)
